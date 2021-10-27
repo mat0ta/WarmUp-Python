71b78bd339a42e81256797e0b880d7c4a2f541e4
Matota <72810000+mat0ta@users.noreply.github.com>
2021-10-27 10:48:09 +0200

    Added Tarea 7

--- script.py
+++ script.py
@@ -2,27 +2,6 @@
 import random
 import datetime
 
-# Tarea 6
-# Importamos shipping.py
-customer_basket_cost = 101
-customer_basket_weight = 44
-
-if float(customer_basket_cost) > 100.0:
-    print('El precio total de la compra es de: ' + str(customer_basket_cost) + '€ (Envío gratutito)')
-else:
-    shippingPrice = float(customer_basket_weight) * 1.2
-    totalPrice = float(customer_basket_cost) + shippingPrice
-    print('El precio total ' + str(totalPrice) + '€ (Precio de envio: ' + str(shippingPrice) + ')')
-
-# Diagrama creado: https://matota.xyz/assets/uploads/uax/programacion-computadores/diagrama-tarea1.png
-
-# Se observa como cuando se ejecuta el código con un valor de customer_basket_cost inferior a 100 te responde añadiendo el valor del coste de shipping mientras que 
-# cuando es superior, te lo da como gratutito.
-
-#Tarea 7
-for i in range(0, 51):
-    print(i)
-
 # Importamos el código de la Tarea 5
 edad = random.randint(0, 50)
 print(edad)
