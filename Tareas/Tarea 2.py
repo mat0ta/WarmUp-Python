4301db7984f1d8f90c53e1dea3add82a97a1775d
Matota <72810000+mat0ta@users.noreply.github.com>
2021-10-27 10:59:37 +0200

    Update Tarea 2.py

--- Tareas/Tarea 2.py
+++ Tareas/Tarea 2.py
@@ -1,1 +1,8 @@
 print("\033[92mHello World\033[0m") # Empleamos el codigo de color ANSI de Python para cambiar el color del Texto al ser printeado en la consola.
+
+# Usamos colorama para realizar el mismo ejercicio pero empleando un módula para el cambio de colores
+import colorama
+from colorama import Fore as color
+colorama.init(autoreset=True)
+
+print(color.GREEN+"Hello world")
