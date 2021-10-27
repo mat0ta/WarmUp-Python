15fc43cccfe40bcd985211384631d7830b413085
Matota <72810000+mat0ta@users.noreply.github.com>
2021-10-27 10:47:14 +0200

    Update Tarea 6.py

--- Tarea 6.py
+++ Tarea 6.py
@@ -8,3 +8,8 @@ else:
     shippingPrice = float(customer_basket_weight) * 1.2
     totalPrice = float(customer_basket_cost) + shippingPrice
     print('El precio total ' + str(totalPrice) + '€ (Precio de envio: ' + str(shippingPrice) + ')')
+
+# Diagrama creado: https://matota.xyz/assets/uploads/uax/programacion-computadores/diagrama-tarea1.png
+
+# Se observa como cuando se ejecuta el código con un valor de customer_basket_cost inferior a 100 te responde añadiendo el valor del coste de shipping mientras que 
+# cuando es superior, te lo da como gratutito.
