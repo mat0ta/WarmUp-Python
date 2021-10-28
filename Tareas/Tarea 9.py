97acd38ed8879d2b348ccafa3aa15b45e179939c
Matota <72810000+mat0ta@users.noreply.github.com>
2021-10-28 10:11:42 +0200

    Updated Tarea 9 (Fixed Directory)

--- Tareas/Tarea 9.py
+++ Tareas/Tarea 9.py
@@ -1,9 +1,9 @@
 # Creamos el archivo flag.txt
-f = open('./flag.txt', 'a')
+f = open('../flag.txt', 'a')
 f.write("Este es un documento .txt\n")
 f.close()
 
-f = open('./flag.txt', 'r')
+f = open('../flag.txt', 'r')
 print(f.read())
 f.close()
 
