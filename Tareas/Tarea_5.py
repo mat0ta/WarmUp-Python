070b29046c06a86b0ab2d894ff0f2e70dcd50843
Matota <72810000+mat0ta@users.noreply.github.com>
2021-10-29 10:31:23 +0200

    Update Tarea_5.py

--- Tareas/Tarea_5.py
+++ Tareas/Tarea_5.py
@@ -2,7 +2,8 @@ dinero = 2000
 precioHelado = 100
 edad = float(input('¿Cual es tu edad?: '))
 hambre = edad
-helado = 20 # Suponemos que el helado te satisface un 20%
+helado = edad
+sinDinero = False
 
 if edad >= 85:
     print('No tienes hambre')
@@ -12,17 +13,22 @@ else:
         cantidadAComer = float(satisfaccion) / float(helado)
         cantidadAComer = cantidadAComer.__round__() + 1
         for i in range(1, cantidadAComer + 1):
-            print(i)
-            dinero = float(dinero) - float(precioHelado)
-            precioAñadido = float(precioHelado) * 0.20
-            precioHelado += float(precioAñadido)
-            hambre += helado
+            if dinero >= precioHelado:
+                print(i)
+                dinero = float(dinero) - float(precioHelado)
+                precioAñadido = float(precioHelado) * 0.20
+                precioHelado += float(precioAñadido)
+                hambre += helado
+            else:
+                sinDinero = True
         if cantidadAComer == 1:
             print('Has comido 1 helado. Ahora estás un ' + str(hambre) + '%' + ' satisfecho y tienes ' + str(dinero) + '€; por lo que:')
         else:
             print('Has comido ' + str(cantidadAComer) + ' helados. Ahora estás un ' + str(hambre) + '%' + ' satisfecho y tienes ' + str(dinero) + '€; por lo que:')
-        if float(hambre) < 85.0:
+        if float(hambre) < 85.0 and not sinDinero:
             print('Sigues teniendo hambre')
+        elif float(hambre) < 85.0 and sinDinero:
+            print('Sigues teniendo hambre porque te has quedado sin dinero')
         elif float(hambre) >= 85.0 and float(hambre) <= 100.0:
             print('Ya no tienes hambre')
         elif float(hambre) > 100.0:
