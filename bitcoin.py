7ca9982fe4614f92751086494270ff7e19f15c1d
Matota <72810000+mat0ta@users.noreply.github.com>
2021-10-29 08:58:31 +0200

    Update bitcoin.py

--- bitcoin.py
+++ bitcoin.py
@@ -2,21 +2,21 @@
 from typing_extensions import runtime
 
 # Declaramos las variables
-investment_in_bitcoin = 1.2
 bitcoin_to_usd= 63000
 usd_to_auro = 0.86
 
 # Creamos la función para pasar de Bitcoin a USD y a su vez a EURO
 def usdToEuro():
-    bitcoinToUsd = (float(investment_in_bitcoin) * int(bitcoin_to_usd))
+    bitcoinAmount = input('Introduce la cantidad de Bitcoin que posees: ')
+    while not str(bitcoinAmount).replace('.', '', 1).isdigit() and not str(bitcoinAmount).isdigit():
+        bitcoinAmount = input('Introduce una cantidad apropiada: ')
+    bitcoinToUsd = (float(bitcoinAmount) * int(bitcoin_to_usd))
     bitcoinToEuro = (float(bitcoinToUsd) * float(usd_to_auro))
-    print("Posees " + str(investment_in_bitcoin) + " Bitcoin(s) valorados en " + str(bitcoinToEuro) + " €")
+    print("Posees " + str(bitcoinAmount) + " Bitcoin(s) valorados en " + str(bitcoinToEuro) + " €")
 
     if float(bitcoinToEuro) < float(30000.0):
-        print("Alerta: El valor de tu(s) Bitcoin(s) está por debajo de los 30,000 €")
-    else:
-        print("Alerta: El valor de tu(s) Bitcoin(s) está por encima de los 30,000 €")
+        print("Alerta: El valor de tu(s) Bitcoin(s) está por debajo de los 30.000 €")
 
-usdToEuro()
+usdToEuro() 
 # Output: Posees 1.2 Bitcoin(s) valorados en 65016.0 €
 #         Alerta: El valor de tu(s) Bitcoin(s) está por encima de los 30,000 €
